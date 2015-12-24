670e597bc9f4ae279b10f089c8d39f9a9696e66a
cxhy <979009228@qq.com>
2015-12-24 23:15:09 +0800

    修正了把地址错误解析到code_ctrl寄存器上的bug

--- dma_tfbuffer.v
+++ dma_tfbuffer.v
@@ -29,7 +29,7 @@
 // $Rev:  $
 // $CreatDate:   2015-11-19 15:21:53
 // $LastChangedBy: guodezheng $
-// $LastChangedDate:  2015-12-24 10:00:14
+// $LastChangedDate:  2015-12-24 23:13:15
 //----------------------------------------------------------------------------
 //
 // *File Name: dma_tfbuffer.v
@@ -79,18 +79,31 @@ module dma_tfbuffer(
                          DECODER_BUFFEROUT           = 'h02,
                          CODE_CTRLOUT                = 'h04;
 
-parameter                DEC_SZ  = (1 << DEC_WD);
-parameter [DEC_SZ-1:0]   DEC_REG = {{DEC_SZ-1{1'b0}},1'b1};
+parameter                DEC_SZ  = (1 << DEC_WD);                                   //8
+parameter [DEC_SZ-1:0]   DEC_REG = {{DEC_SZ-1{1'b0}},1'b1};                         //0000_0001
 
-parameter [DEC_SZ-1:0]   ENCODER_BUFFERIN_D  = (DEC_REG << ENCODER_BUFFERIN),
-                         DECODER_BUFFEROUT_D = (DEC_REG << DECODER_BUFFEROUT),
-                         CODE_CTRLOUT_D      = (DEC_REG << CODE_CTRLOUT);
+parameter [DEC_SZ-1:0]   ENCODER_BUFFERIN_D  = (DEC_REG << ENCODER_BUFFERIN),       //0000_0001
+                         DECODER_BUFFEROUT_D = (DEC_REG << DECODER_BUFFEROUT),      //0000_0100
+                         CODE_CTRLOUT_D      = (DEC_REG << CODE_CTRLOUT);           //0001_0000
 
 wire           reg_sel          = per_en & (per_addr [13:DEC_WD-1] == BASE_ADDR[14:DEC_WD]);
-wire      [DEC_WD-1:0] reg_addr = {1'b0,per_addr[DEC_WD-2:0]};
-wire      [DEC_SZ-1:0] reg_dec  = (ENCODER_BUFFERIN_D         & {DEC_SZ{(reg_addr == ( ENCODER_BUFFERIN       >>1))}})|
-                                  (DECODER_BUFFEROUT_D        & {DEC_SZ{(reg_addr == ( DECODER_BUFFEROUT      >>1))}})|
-                                  (CODE_CTRLOUT_D             & {DEC_SZ{(reg_addr == ( CODE_CTRLOUT           >>1))}});
+
+wire      [DEC_WD-1:0] reg_addr = {1'b0, per_addr[DEC_WD-2:0]};        // addr        per_addr       reg_addr
+                                                                       // 01aa        00d5           0_01
+                                                                       // 01ac        00d6           0_10
+                                                                       // 01ae        00d7           0_11
+
+parameter [DEC_WD-1:0]  reg_1 = 3'b001,
+                        reg_2 = 3'b010,
+                        reg_3 = 3'b011;
+
+wire      [DEC_SZ-1:0] reg_dec  = (ENCODER_BUFFERIN_D         & {DEC_SZ{(reg_addr == reg_1)}})|       //00           >>1 == 00
+                                  (DECODER_BUFFEROUT_D        & {DEC_SZ{(reg_addr == reg_2)}})|       //02           >>1 == 01
+                                  (CODE_CTRLOUT_D             & {DEC_SZ{(reg_addr == reg_3)}});       //04           >>1 == 10
+
+// wire      [DEC_SZ-1:0] reg_dec  = (ENCODER_BUFFERIN_D         & {DEC_SZ{(reg_addr == ( ENCODER_BUFFERIN       >>1))}})|       //00           >>1 == 00
+//                                   (DECODER_BUFFEROUT_D        & {DEC_SZ{(reg_addr == ( DECODER_BUFFEROUT      >>1))}})|       //02           >>1 == 01
+//                                   (CODE_CTRLOUT_D             & {DEC_SZ{(reg_addr == ( CODE_CTRLOUT           >>1))}});       //04           >>1 == 10
 
 wire              reg_lo_write =  per_we[0] & reg_sel;
 wire              reg_hi_write =  per_we[1] & reg_sel;
@@ -100,67 +113,24 @@ wire [DEC_SZ-1:0] reg_hi_wr    = reg_dec & {DEC_SZ{reg_hi_write}};
 wire [DEC_SZ-1:0] reg_lo_wr    = reg_dec & {DEC_SZ{reg_lo_write}};
 wire [DEC_SZ-1:0] reg_rd       = reg_dec & {DEC_SZ{reg_read}};
 
-
+//decoder_reg
 reg  [7:0] decoder_reg;
 
 wire       decoder_reg_wr  = DECODER_BUFFEROUT[0] ? reg_hi_wr[DECODER_BUFFEROUT]   : reg_lo_wr[DECODER_BUFFEROUT];
-wire [7:0] decoder_reg_nxt;
+wire [7:0] decoder_reg_nxt = DECODER_BUFFEROUT[0] ? per_din[15:8]                  : per_din[7:0];
 // wire [7:0] decoder_reg_nxt_dly1 <= decoder_reg_nxt;
-// wire [7:0] decoder_reg_nxt_dly2 <= decoder_reg_nxt_dly1;
-// wire   [7:0] decoder_reg_nxt;
-// always @(posedge mclk or posedge puc_rst) begin
-// 	if (puc_rst) begin
-// 		// reset
-//         decoder_reg_nxt <= 8'b0;
-// 	end
-// 	else if () begin
-
-// 	end
-// end
-reg [7:0] decoder_reg_nxt_reg;
-reg [7:0] decoder_reg_nxt_reg_dly1;
-reg [7:0] decoder_reg_nxt_reg_dly2;
-reg [7:0] decoder_reg_nxt_reg_dly3;
-reg [7:0] decoder_reg_nxt_reg_dly4;
-reg [7:0] decoder_reg_nxt_reg_dly5;
-reg [7:0] decoder_reg_nxt_reg_dly6;
-reg [7:0] decoder_reg_nxt_reg_dly7;
-reg [7:0] decoder_reg_nxt_reg_dly8;
-reg [7:0] decoder_reg_nxt_reg_dly9;
-reg [7:0] decoder_reg_nxt_reg_dly10;
-
 always @(posedge mclk or posedge puc_rst) begin
-	if (puc_rst) begin
-		// reset
-		decoder_reg_nxt_reg <= 8'b0;
-	end
-	else begin
-        decoder_reg_nxt_reg       <=  per_din[7:0];
-        decoder_reg_nxt_reg_dly1  <= decoder_reg_nxt_reg;
-        decoder_reg_nxt_reg_dly2  <= decoder_reg_nxt_reg_dly1;
-        decoder_reg_nxt_reg_dly3  <= decoder_reg_nxt_reg_dly2;
-        decoder_reg_nxt_reg_dly4  <= decoder_reg_nxt_reg_dly3;
-        decoder_reg_nxt_reg_dly5  <= decoder_reg_nxt_reg_dly4;
-        decoder_reg_nxt_reg_dly6  <= decoder_reg_nxt_reg_dly5;
-        decoder_reg_nxt_reg_dly7  <= decoder_reg_nxt_reg_dly6;
-        decoder_reg_nxt_reg_dly8  <= decoder_reg_nxt_reg_dly7;
-        decoder_reg_nxt_reg_dly9  <= decoder_reg_nxt_reg_dly8;
-        decoder_reg_nxt_reg_dly10 <= decoder_reg_nxt_reg_dly9;
-	end
+	if (puc_rst)            decoder_reg <= 8'b0;
+	else if(decoder_reg_wr) decoder_reg <= decoder_reg_nxt;
 end
 
-assign decoder_reg_nxt = decoder_reg_nxt_reg_dly1;
+assign decoder_buffer_dout = decoder_reg;
 
-always @(posedge mclk or posedge puc_rst)
-	if (puc_rst)                decoder_reg <= 8'h0;
-	else if (decoder_reg_wr)    decoder_reg <= decoder_reg_nxt;
-
-assign decoder_buffer_dout = decoder_reg ;
-
+//code_ctrl_reg
 reg  [7:0] code_ctrl_reg;
 
-wire       code_ctrl_reg_wr  = CODE_CTRLOUT_D[0] ? reg_hi_wr[CODE_CTRLOUT] : reg_lo_wr[CODE_CTRLOUT];
-wire [7:0] code_ctrl_reg_nxt = CODE_CTRLOUT[0] ? per_din[15:8]                  : per_din[7:0];
+wire       code_ctrl_reg_wr  = CODE_CTRLOUT[0] ? reg_hi_wr[CODE_CTRLOUT] : reg_lo_wr[CODE_CTRLOUT];
+wire [7:0] code_ctrl_reg_nxt = CODE_CTRLOUT[0]   ? per_din[15:8]                  : per_din[7:0];
 
 always @(posedge mclk or posedge puc_rst)
     if (puc_rst)                   code_ctrl_reg <= 8'h0;
